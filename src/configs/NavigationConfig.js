731991ea1a485ab4a8499cf8ff21fb7f87255837
Willian <willian.buqui@gmail.com>
2022-04-16 20:09:45 -0300

    iniciando

---
+++ src/configs/NavigationConfig.js
@@ -0,0 +1,73 @@
+import { 
+  AppstoreOutlined,
+  DashboardOutlined,
+  ShoppingCartOutlined,
+} from '@ant-design/icons';
+import { APP_PREFIX_PATH } from 'configs/AppConfig'
+
+const dashBoardNavTree = [{
+  key: 'home',
+  path: `${APP_PREFIX_PATH}/home`,
+  title: 'home',
+  icon: DashboardOutlined,
+  breadcrumb: false,
+  submenu: []
+}]
+
+const appsNavTree = [{
+  key: 'crm',
+  path: `${APP_PREFIX_PATH}/crm`,
+  title: 'sidenav.apps',
+  icon: AppstoreOutlined,
+  breadcrumb: false,
+  submenu: [
+    {
+      key: 'apps-ecommerce',
+      path: `${APP_PREFIX_PATH}/crm/ecommerce`,
+      title: 'sidenav.apps.ecommerce',
+      icon: ShoppingCartOutlined,
+      breadcrumb: true,
+      submenu: [
+        {
+          key: 'apps-ecommerce-productList',
+          path: `${APP_PREFIX_PATH}/crm/ecommerce/product-list`,
+          title: 'sidenav.apps.ecommerce.productList',
+          icon: '',
+          breadcrumb: true,
+          submenu: []
+        },
+        {
+          key: 'apps-ecommerce-addProduct',
+          path: `${APP_PREFIX_PATH}/crm/ecommerce/add-product`,
+          title: 'sidenav.apps.ecommerce.addProduct',
+          icon: '',
+          breadcrumb: false,
+          submenu: []
+        },
+        {
+          key: 'apps-ecommerce-editProduct',
+          path: `${APP_PREFIX_PATH}/crm/ecommerce/edit-product/12`,
+          title: 'sidenav.apps.ecommerce.editProduct',
+          icon: '',
+          breadcrumb: false,
+          submenu: []
+        },
+        {
+          key: 'apps-ecommerce-orders',
+          path: `${APP_PREFIX_PATH}/crm/ecommerce/orders`,
+          title: 'sidenav.apps.ecommerce.orders',
+          icon: '',
+          breadcrumb: false,
+          submenu: []
+        }
+      ]
+    }
+  ]
+}]
+
+const navigationConfig = [
+  ...dashBoardNavTree,
+  ...appsNavTree,
+]
+
+export default navigationConfig;
